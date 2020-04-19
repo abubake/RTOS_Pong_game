cd1acfe44aaf931f526e8e4cc12df623fd384ffb
steven <stevenrosenthal@ufl.edu>
2020-04-19 15:40:03 -0400

    new main.c w/ aperiodics and Wait Screen Thread

--- main.c
+++ main.c
@@ -22,47 +22,6 @@ int hostFlag = 0x55;
 test_t winning;
 
 void main(void){
-
-
-	//initCC3100(Host);
-
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; //
-
-	winning.greatSucsess = 55;
-	initCC3100(Host);
-
-	/* Initializes all the hardware resources on the board and sets the amount of threads and system time to 0 */
-/*
-	uint8_t ip_addy[4];
-
-	int retval = -1;
-	while(retval < 0){
-		retval = ReceiveData(ip_addy, 4);
-	}
-
-	const uint8_t bufferSize = 4;
-	unsigned char buffer[bufferSize] = {4, 0, 0 , 4};
-
-
-	uint32_t client_IP = 0;
-
-
-	for(uint8_t i = 0; i < bufferSize; i++){
-		client_IP = client_IP | (uint32_t)(ip_addy[i]<<8*(bufferSize - 1 - i));
-	}
-
-	SendData(buffer, client_IP, 4);
-*/
-	//while(1);
-/*
-	P4DIR &= ~BIT4;
-	P4IFG &= ~BIT4; // clear interrupt flag bit
-	P4IE |= BIT4; // enable interrupt on pin 4
-	P4IES |= BIT4; //enables low to high transition
-	P4REN |= BIT4; // pull up resistor
-	P4OUT |= BIT4; // sets resistor to pull up
-*/
-
 	G8RTOS_Init();
 
 	LCD_Init(false);
@@ -74,23 +33,29 @@ void main(void){
 	G8RTOS_InitSemaphore(&USING_SPI, 1);
 	G8RTOS_InitSemaphore(&USING_LED_I2C, 1);
 
-
+	/*Aperioidic Threads*/
+	//Button taps
     P4->SEL1 &= ~BIT4;
     P4->SEL0 &= ~BIT4;
-    P4->DIR &= ~BIT4;
-    P4->IFG &= ~BIT4;   //Clears P4.4
-    P4->IE |= BIT4;     //Enables interrupt
-    P4->IES |= BIT4;    //High to low transition
-    P4->REN |= BIT4;    //Pull-up resistor
-    P4->OUT |= BIT4;    //Sets res to pull-up
-    NVIC_SetPriority(PORT4_IRQn, 6);
-    NVIC_EnableIRQ(PORT4_IRQn);
+    P4->DIR  &= ~BIT4;
+    P4->IFG  &= ~BIT4;   //Clears P4.4
+    P4->IE   |= BIT4;     //Enables interrupt
+    P4->IES  |= BIT4;    //High to low transition
+    P4->REN  |= BIT4;    //Pull-up resistor
+    P4->OUT  |= BIT4;    //Sets res to pull-up
+
+    P5->SEL1 &= ~BIT2;
+    P5->SEL0 &= ~BIT2;
+    P5->DIR  &= ~BIT2;
+    P5->IFG  &= ~BIT2;   //Clears P5.2
+    P5->IE   |= BIT2;    //Enables interrupt
+    P5->IES  |= BIT2;    //High to low transition
+    P5->REN  |= BIT2;    //Pull-up resistor
+    P5->OUT  |= BIT2;    //Sets res to pull-up
+
+    G8RTOS_AddThread(WaitScreen, 1, "Wait_Screen");
     G8RTOS_AddAPeriodicEvent(TOP_BUTTON_TAP, 6, PORT4_IRQn);
-
-
-	/* Adds each task individually to the system */
-	G8RTOS_AddThread(CreateGame, 150, "CreateGame"); //NEEDS real PRI and maybe better nam
-	//G8RTOS_AddAPeriodicEvent(LCD_Tap, 3,  PORT4_IRQn);
+    G8RTOS_AddAPeriodicEvent(BOTTOM_BUTTON_TAP, 6, PORT5_IRQn);
 
 	G8RTOS_InitFIFO(JOYSTICKFIFO);
 
@@ -99,15 +64,6 @@ void main(void){
 	G8RTOS_Launch();
 
 }
-
-/*
-void PORT4_IRQHandler(void){
-        P4IE &= ~BIT4; // disables interrupt on pin 4
-        P4IFG &= ~BIT4; // must clear IFG flag to operate
-        P4IE |= BIT4; // enable interrupt on pin 4
-    }
-*/
-
 #endif /* MAIN_LAB5 */
 
 
