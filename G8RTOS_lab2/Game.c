c44e0fc518a80e04f49e65abaa69a9dca9ea96d3
BakerHerrin <bakerherrin2@gmail.com>
2020-04-21 19:35:33 -0400

    neter

--- G8RTOS_lab2/Game.c
+++ G8RTOS_lab2/Game.c
@@ -99,7 +99,7 @@ void JoinGame(){
     G8RTOS_AddThread(ReadJoystickClient, 3, "ReadJoystickClient");
     G8RTOS_AddThread(SendDataToHost, 3, "SendDataToHost");
     G8RTOS_AddThread(ReceiveDataFromHost, 3, "ReceiveDataFromHost");
-    //G8RTOS_AddThread(DrawObjects, 3, "DrawObjects");
+    G8RTOS_AddThread(DrawObjects, 3, "DrawObjects");
     //G8RTOS_AddThread(MoveLEDs, 250, "MoveLEDs");
     //G8RTOS_AddThread(IdleThread, 250, "idle");
     DelayMs(1);
@@ -318,7 +318,7 @@ void CreateGame(){
 	/* Add these threads. (Need better priority definitions) */
     //G8RTOS_AddThread(GenerateBall, 2, "GenerateBall");
     G8RTOS_AddThread(ReceiveDataFromClient, 3, "ReceiveDataFromClient");
-    //G8RTOS_AddThread(DrawObjects, 5, "DrawObjects");
+    G8RTOS_AddThread(DrawObjects, 3, "DrawObjects");
     G8RTOS_AddThread(ReadJoystickHost, 3, "ReadJoystickHost");
     G8RTOS_AddThread(SendDataToClient, 3, "SendDataToClient");
     //G8RTOS_AddThread(MoveLEDs, 250, "MoveLEDs"); //lower priority
